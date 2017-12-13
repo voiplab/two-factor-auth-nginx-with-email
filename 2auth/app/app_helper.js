63967e3e7595bcc257cf8eb24737ddb05acc9100
Nikita Rukavkov <nrukavkov@appulate.com>
2017-12-13 17:12:01 +0300

    Formatting

--- 2auth/app/app_helper.js
+++ 2auth/app/app_helper.js
@@ -1,116 +1,126 @@
-var dirty = require('dirty');
-var db = dirty('user.db');
-const fs = require('fs');
-var moment = require('moment');
-var config = require('./config');
-var uuid = require('uuid');
-var validator = require('validator');
-var nodemailer = require('nodemailer');
-
+var dirty = require("dirty");
+var db = dirty("user.db");
+const fs = require("fs");
+var moment = require("moment");
+var config = require("./config");
+var uuid = require("uuid");
+var validator = require("validator");
+var nodemailer = require("nodemailer");
 
 ////////////////// MAIL HELPER /////////////////////////////////
 //Send message
 exports.mail_sendMessage = function(emailAddress, code, cb) {
-    let transporter = nodemailer.createTransport(config.mail_server);
-    transporter.sendMail(exports.mail_getOptions(config.mail_from, emailAddress, config.mail_title, config.mail_message_auth_url, code), (error, info) => {
-        if (error) {
-            console.log(error);
-            cb(false);
-            return;
-        }
-        console.log('Message sent: %s', info.messageId);
-        cb(true);
-    });
-}
+  let transporter = nodemailer.createTransport(config.mail_server);
+  transporter.sendMail(
+    exports.mail_getOptions(
+      config.mail_from,
+      emailAddress,
+      config.mail_title,
+      config.mail_message_auth_url,
+      code
+    ),
+    (error, info) => {
+      if (error) {
+        console.log(error);
+        cb(false);
+        return;
+      }
+      console.log("Message sent: %s", info.messageId);
+      cb(true);
+    }
+  );
+};
 
 //Generating email
 exports.mail_getOptions = function(from, to, subject, base_url, code) {
-    return {
-        from: from,
-        to: to,
-        subject: subject,
-        html: `Use the link to get access: <a href=${base_url}/confirm/${code}>Click me</a>`
-    };
-}
+  return {
+    from: from,
+    to: to,
+    subject: subject,
+    html: `Use the link to get access: <a href=${base_url}/confirm/${code}>Click me</a>`
+  };
+};
 
 //Email validation
 exports.mail_validateEmail = function(email) {
-    return validator.isEmail(email, {
-        allow_utf8_local_part: false
-    });
-}
+  return validator.isEmail(email, {
+    allow_utf8_local_part: false
+  });
+};
 
 ////////////////// DB HELPER /////////////////////////////////
 
 exports.db_showall = function() {
-    db.forEach(function(key, val) {
-        console.log('Found key: %s, val: %j', key, val);
-    });
-}
+  db.forEach(function(key, val) {
+    console.log("Found key: %s, val: %j", key, val);
+  });
+};
 
 exports.db_writeConfig = function() {
-    let file_auth_content = '';
-    db.forEach(function(key, val) {
-        if (val.grant_permission == true) {
-            file_auth_content = file_auth_content + `${key} 1;\n`;
-            console.log('Content: %s', file_auth_content);
-        }
-    });
+  let file_auth_content = "";
+  db.forEach(function(key, val) {
+    if (val.grant_permission == true) {
+      file_auth_content = file_auth_content + `${key} 1;\n`;
+      console.log("Content: %s", file_auth_content);
+    }
+  });
 
-    //Write nginx config
-    fs.readFile(config.app_nginx_filename, 'utf8', function (err,data) {
+  //Write nginx config
+  fs.readFile(config.app_nginx_filename, "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    if (data != file_auth_content) {
+      fs.writeFile(config.app_nginx_filename, file_auth_content, err => {
         if (err) {
           return console.log(err);
         }
-        if (data != file_auth_content)
-        {
-            fs.writeFile(config.app_nginx_filename, file_auth_content, (err) => {
-                if (err) { 
-                    return console.log(err); 
-                }
-                console.log(`nginx: config saved!`);
-            });
-        }
-        else
-        {
-            console.log('nginx: nothing changed');
-        }
+        console.log(`nginx: config saved!`);
       });
-    
-    exports.db_showall();
-}
+    } else {
+      console.log("nginx: nothing changed");
+    }
+  });
 
+  exports.db_showall();
+};
 
 exports.db_push = function(ip, grant_permission) {
-    let code = uuid.v1();
-    db.set(ip, {
-        code,
-        grant_permission,
-        datetime: moment().format()
-    }, function() {
-        console.log('Code %s for IP address %s', code, ip);
-    });
-    return code;
-}
+  let code = uuid.v1();
+  db.set(
+    ip,
+    {
+      code,
+      grant_permission,
+      datetime: moment().format()
+    },
+    function() {
+      console.log("Code %s for IP address %s", code, ip);
+    }
+  );
+  return code;
+};
 
 exports.db_verifyCode = function(ip, code) {
-    return (code == db.get(ip).code) && !db.get(ip).grant_permission;
-}
+  return code == db.get(ip).code && !db.get(ip).grant_permission;
+};
 
 exports.db_disableAccessCron = function() {
-    db.forEach(function(key, val) {
-        if (val.grant_permission == true) {
-            if (moment().diff(val.datetime, 'minutes') > config.app_expire_time)
-            //Generate a new uuid to disable reusing a link into mailbox
-                exports.db_push(key, false)
-            exports.db_writeConfig();
-        }
-    });
-}
+  db.forEach(function(key, val) {
+    if (val.grant_permission == true) {
+      if (moment().diff(val.datetime, "minutes") > config.app_expire_time)
+        //Generate a new uuid to disable reusing a link into mailbox
+        exports.db_push(key, false);
+      exports.db_writeConfig();
+    }
+  });
+};
 
 ////////////////////// COMMON HELPER ////////////////////
 
 //Find client IP address
 exports.getClientIP = function(req) {
-    return (req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace(/^.*:/, '');
-}
+  return (
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress
+  ).replace(/^.*:/, "");
+};
--- 2auth/app/routes/index.js
+++ 2auth/app/routes/index.js
@@ -1,70 +1,69 @@
-var validator = require('validator');
-var config = require('../config');
-var express = require('express');
+var validator = require("validator");
+var config = require("../config");
+var express = require("express");
 var router = express.Router();
-var dirty = require('dirty');
-var db = dirty('user.db');
-var helper = require('../app_helper');
+var dirty = require("dirty");
+var db = dirty("user.db");
+var helper = require("../app_helper");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
+  let ip = helper.getClientIP(req);
+  res.render("index", {
+    title: config.ui_title,
+    domain: config.ui_domain,
+    ip_address: ip
+  });
+});
+
+router.post("/", function(req, res, next) {
+  let mailbox = req.body.mailbox.concat("@" + config.ui_domain);
+
+  if (helper.mail_validateEmail(mailbox)) {
     let ip = helper.getClientIP(req);
-    res.render('index', {
-        title: config.ui_title,
-        domain: config.ui_domain,
-        ip_address: ip
+    let code = helper.db_push(ip, false);
+
+    helper.mail_sendMessage(mailbox, code, sent => {
+      if (sent) {
+        res.render("index", {
+          title: config.ui_title,
+          maillink: config.ui_mail_link,
+          mailbox: mailbox
+        });
+      } else {
+        res.render("message", {
+          title: config.ui_title,
+          message: config.ui_message_send_mail_failed
+        });
+      }
     });
+  } else {
+    res.render("message", {
+      title: config.company_name,
+      message: config.ui_message_validator_email_error
+    });
+  }
 });
 
-router.post('/', function(req, res, next) {
-    let mailbox = req.body.mailbox.concat('@' + config.ui_domain);
+router.get("/confirm/:code", function(req, res, next) {
+  let code = req.params["code"];
+  let ip = helper.getClientIP(req);
 
-    if (helper.mail_validateEmail(mailbox)) {
-        let ip = helper.getClientIP(req);
-        let code = helper.db_push(ip, false);
-
-        helper.mail_sendMessage(mailbox, code, sent => {
-            if (sent) {
-                res.render('index', {
-                    title: config.ui_title,
-                    maillink: config.ui_mail_link,
-                    mailbox: mailbox
-    
-                });
-            } else {
-                res.render('message', {
-                    title: config.ui_title,
-                    message: config.ui_message_send_mail_failed
-                });
-            }
-        });
-    } else {
-        res.render('message', {
-            title: config.company_name,
-            message: config.ui_message_validator_email_error
-        });
-    }
-});
-
-router.get('/confirm/:code', function(req, res, next) {
-    let code = req.params['code'];
-    let ip = helper.getClientIP(req);
-
-    //Request IP address should equal IP address code confirmation
-    if (helper.db_verifyCode(ip, code)) {
-        //Adding to white list
-        helper.db_push(ip, true);
-        helper.db_writeConfig();
-        res.render('message', {
-            title: config.ui_title,
-            message: config.ui_message_confirm_success
-        });
-    } else {
-        res.render('message', {
-            title: config.ui_title,
-            message: config.ui_message_confirm_fail
-        });
-    }
+  //Request IP address should equal IP address code confirmation
+  if (helper.db_verifyCode(ip, code)) {
+    //Adding to white list
+    helper.db_push(ip, true);
+    helper.db_writeConfig();
+    res.render("message", {
+      title: config.ui_title,
+      message: config.ui_message_confirm_success
+    });
+  } else {
+    res.render("message", {
+      title: config.ui_title,
+      message: config.ui_message_confirm_fail
+    });
+  }
 });
 
 module.exports = router;
